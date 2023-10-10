c01e6b8bfc2e9249c4707ba7549e692dac38c8c6
NerfedJabolo <ezdlprofit1@gmail.com>
2023-10-10 19:37:31 +0300

    remove deep recursion, optimize content processing

--- src/script.js
+++ src/script.js
@@ -1,88 +1,45 @@
 window.addEventListener('DOMContentLoaded', async () => {
-  // Create a cache object to store fetched content promises.
   const cache = {};
 
-  async function includeHTML(element) {
-    // Get the 'data-include' attribute from the given 'element'.
-    const file = element.getAttribute('data-include');
+  async function includeHTML(elements) {
+    const promises = elements.map(async (element) => {
+      const file = element.getAttribute('data-include');
+      if (!cache[file]) {
+        cache[file] = fetch(file).then((response) => response.text());
+      }
 
-    // If not in cache, store the fetch promise
-    if (!cache[file]) {
-      cache[file] = fetch(file).then((response) => response.text());
-    }
+      const html = await cache[file];
+      const template = document.createElement('template');
+      template.innerHTML = html.trim();
 
-    // Await the cached promise
-    const html = await cache[file];
-
-    // Create a template element and set its HTML content to the fetched HTML.
-    const template = document.createElement('template');
-    template.innerHTML = html.trim();
-
-    // Clone the content of the template.
-    const content = template.content.cloneNode(true);
-
-    // Get the parent element of the 'element'.
-    const parentElement = element.parentElement;
-
-    // Check if a parent element exists.
-    if (parentElement) {
-      // Get the content within the current 'element'.
-      const componentContent = element.innerHTML.trim();
-
-      // Find all elements with a 'data-content' attribute within the cloned content.
-      const contentElements = content.querySelectorAll('[data-content]');
-
-      // Create an array of promises to modify the content elements.
-      const promises = Array.from(contentElements, (contentElement) => {
-        return new Promise((resolve) => {
-          // Determine the type of the content element (e.g., input, textarea).
+      if (element.parentElement) {
+        const componentContent = element.innerHTML.trim();
+        const contentElements =
+          template.content.querySelectorAll('[data-content]');
+        contentElements.forEach((contentElement) => {
           const elementType = contentElement.tagName.toLowerCase();
-
-          // Set the content of the content element based on its type.
           contentElement[
             elementType === 'input' || elementType === 'textarea'
               ? 'value'
               : 'innerHTML'
           ] = componentContent;
-
-          // Resolve the promise.
-          resolve();
         });
-      });
 
-      // Wait for all promises to complete.
-      await Promise.all(promises);
+        const fragment = document.createDocumentFragment();
+        fragment.appendChild(template.content);
+        element.parentElement.insertBefore(fragment, element);
+        element.remove();
+      }
+    });
 
-      // Create a document fragment to hold the cloned nodes.
-      const fragment = document.createDocumentFragment();
-
-      // Convert the cloned nodes to an array.
-      const clonedNodes = Array.from(content.childNodes);
-
-      // Append each cloned node to the fragment.
-      clonedNodes.forEach((node) => fragment.appendChild(node));
-
-      // Insert the fragment before the original 'element'.
-      parentElement.insertBefore(fragment, element);
-
-      // Remove the original 'element' from the DOM.
-      element.remove();
-    }
-
-    // Serialize the inclusion of nested elements
-    const nestedElements = Array.from(
-      document.querySelectorAll('[data-include]')
-    );
-    for (const nestedElement of nestedElements) {
-      await includeHTML(nestedElement);
-    }
+    await Promise.all(promises);
   }
 
-  // Serialize the inclusion of elements
-  const includeElements = Array.from(
+  let elementsToInclude = Array.from(
     document.querySelectorAll('[data-include]')
   );
-  for (const includeElement of includeElements) {
-    await includeHTML(includeElement);
+  while (elementsToInclude.length > 0) {
+    await includeHTML(elementsToInclude);
+    elementsToInclude = Array.from(document.querySelectorAll('[data-include]'));
   }
 });
